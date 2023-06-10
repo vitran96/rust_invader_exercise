1a19884d994bd1e590d1670721641debd620def7
vitran96 <tranuytrieuvi@gmail.com>
2023-06-11 00:07:27 +0700

    set up basic terminal screen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::error::Error;
-
+use crossterm::cursor::{Hide, Show};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{terminal, ExecutableCommand};
 use rusty_audio::Audio;
+use std::error::Error;
+use std::io;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -13,7 +16,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?; // ? -> crash if error
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game loop
+
     // Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
     Ok(())
 }
